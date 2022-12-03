2fdc8172fb2bbc79ad3ef2ab5a781d97e98da141
Olaf Gottschalk <Olaf.Gottschalk@bmwgroup.com>
2022-12-03 09:39:14 +0100

    Last refactoring of day 3

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -2,14 +2,13 @@ class Day03 : Day(3, 2022, "Rucksack Reorganization") {
 
     override fun part1() =
         input
-            .map { it.chunked(it.length / 2) { compartment -> compartment.toSet() } }
+            .map { rucksack -> rucksack.chunked(rucksack.length / 2) { comp -> comp.toSet() } }
             .map { (comp1, comp2) -> (comp1 intersect comp2).single() }
             .sumOf { duplicateItem -> duplicateItem.priority }
 
     override fun part2() =
-        input.chunked(3)
-            .map { elves -> elves.map { it.toSet() } }
-            .map { it.reduce { commonItems, elfItems -> commonItems intersect elfItems }.single() }
+        input.chunked(3) { rucksacks -> rucksacks.map { it.toSet() } }
+            .map { elves -> elves.reduce { common, elf -> common intersect elf }.single() }
             .sumOf { badge -> badge.priority }
 
     private val Char.priority
