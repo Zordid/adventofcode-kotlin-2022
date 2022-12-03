fceba6b6d28da17ab6d1f924446cc247f8633cda
Olaf Gottschalk <Olaf.Gottschalk@bmwgroup.com>
2022-12-03 10:29:30 +0100

    No more imperative statements, no more intermediate lists

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,24 +1,20 @@
 class Day03 : Day(3, 2022, "Rucksack Reorganization") {
 
     override fun part1() =
-        input
+        input.asSequence()
             .map { rucksack -> rucksack.chunked(rucksack.length / 2) }
             .sumOf { compartments -> compartments.singleCommonItem().priority }
 
     override fun part2() =
-        input
+        input.asSequence()
             .chunked(3)
             .sumOf { rucksacks -> rucksacks.singleCommonItem().priority }
 
     private fun Iterable<CharSequence>.singleCommonItem() =
         asSequence().map(CharSequence::toSet).reduce(Set<Char>::intersect).single()
 
-    private val Char.priority
-        get() = when (this) {
-            in 'a'..'z' -> this - 'a' + 1
-            in 'A'..'Z' -> this - 'A' + 27
-            else -> error("$this is not an item")
-        }
+    private val Char.priority get() = prioritiesInOrder.indexOf(this)
+    private val prioritiesInOrder = (listOf('-') + ('a'..'z') + ('A'..'Z'))
 
 }
 
