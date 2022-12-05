96254174ee4bc6e9aefd075398cca1085c490a8d
Olaf Gottschalk <Olaf.Gottschalk@bmwgroup.com>
2022-12-05 10:22:18 +0100

    Refactor day 5

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -4,40 +4,39 @@ class Day05 : Day(5, 2022, "Supply Stacks") {
     private val instructions = inputAsGroups.last().map(::containedNumbers)
 
     override fun part1(): Any {
-        val finalStacks = instructions.fold(initialStacks) { stacks, (q, from, to) ->
-            stacks.mapIndexed { index, stack ->
-                when (index) {
-                    from -> stack.dropLast(q)
-                    to -> stack + stacks[from].takeLast(q).reversed()
-                    else -> stack
-                }
-            }
-        }
-
-        return finalStacks.top().joinToString("")
+        val finalStacks = crateMover(initialStacks, instructions, oneByOne)
+        return finalStacks.top()
     }
 
     override fun part2(): Any {
-        val finalStacks = instructions.fold(initialStacks) { stacks, (q, from, to) ->
+        val finalStacks = crateMover(initialStacks, instructions, enBlock)
+        return finalStacks.top()
+    }
+
+    private fun crateMover(
+        initialStacks: List<List<Char>>,
+        instructions: List<List<Int>>,
+        stackingOrder: (List<Char>) -> List<Char>
+    ): List<List<Char>> =
+        instructions.fold(initialStacks) { stacks, (q, from, to) ->
             stacks.mapIndexed { index, stack ->
                 when (index) {
                     from -> stack.dropLast(q)
-                    to -> stack + stacks[from].takeLast(q)
+                    to -> stack + stackingOrder(stacks[from].takeLast(q))
                     else -> stack
                 }
             }
         }
 
-        return finalStacks.top().joinToString("")
-    }
+    private val enBlock = { crates: List<Char> -> crates }
+    private val oneByOne = { crates: List<Char> -> crates.reversed() }
 
-    private fun List<List<Char>>.top() =
-        mapNotNull { it.lastOrNull() }
+    private fun List<List<Char>>.top() = mapNotNull { it.lastOrNull() }.joinToString("")
 
     private fun createStacks(drawing: List<String>) =
-        listOf(emptyList<Char>()) +  // add one empty stack add index 0 to accommodate indexing
-                drawing.reversed() // turn it upside down
-                    .drop(1)    // and ignore the numbering
+        listOf(emptyList<Char>()) +  // add one empty stack at index 0 to accommodate indexing with "+1"
+                drawing.reversed()   // turn it upside down
+                    .drop(1)      // and ignore the numbering
                     .let { pure ->
                         val columnIndices = pure.first().withIndex().filter { it.value.isLetter() }
                         columnIndices.map { (index, _) ->
